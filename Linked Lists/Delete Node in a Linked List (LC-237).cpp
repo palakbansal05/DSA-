3056307c086e9c4f31350100bfa6d8c5bd2b4200
palakbansal05 <palakbansal5002@gmail.com>
2025-10-22 22:50:45 +0530

    Added solution 2 for Delete Node in a Linked List question in the Linked Lists folder

--- Linked Lists/Delete Node in a Linked List (LC-237).cpp
+++ Linked Lists/Delete Node in a Linked List (LC-237).cpp
@@ -6,10 +6,24 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// Method 1(Preferred):
+
+class Solution {
+public:
+    void deleteNode(ListNode* node){
+// Copy next node's value 
+        node->val=node->next->val;
+// Skip the next node(deleting it from the linked list)
+        node->next=node->next->next;
+    }
+};
+
+// Method 2(Not Preffered):
+
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-// From the element that is to be deleted from the linked list, push the values of the nodes next to them in the current node till we reach the second last node.
+// From the element that is to be deleted from the linked list, copy the values of the nodes next to them in the current node till we reach the second last node.
         while(node->next->next){
             node->val=node->next->val;
             node=node->next;
