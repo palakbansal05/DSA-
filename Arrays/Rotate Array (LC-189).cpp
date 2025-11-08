262913a50cbac1ecaf65bb087264b8423fd4b52a
palakbansal05 <palakbansal5002@gmail.com>
2025-11-08 21:43:45 +0530

    Added solution 2 for Rotate Array in the Arrays folder
    Added brute force and optimized solutions for rotating an array.

--- Arrays/Rotate Array (LC-189).cpp
+++ Arrays/Rotate Array (LC-189).cpp
@@ -1,4 +1,24 @@
-//Method-1
+//Method 1 (Brute Force Solution):
+//Time Complexity: O(n) where n is the size of the input array
+//Space Complexity: o(n)
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+       int n=nums.size();
+       vector<int> res(n);
+       k=k%n;
+       for(int i=0;i<n;i++){
+           int t=(i+k)%n;
+           res[t]=nums[i];
+       }
+       nums=res;
+    }
+};
+
+// Method 2 (Optimized Solution):
+//Time Complexity: O(n) where n is the size of the input array
+//Space Complexity: O(1) 
 
 class Solution {
 public:
