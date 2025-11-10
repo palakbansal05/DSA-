41221d3ef9ada5947f070decfe193099874ba947
palakbansal05 <palakbansal5002@gmail.com>
2025-11-11 01:21:59 +0530

    Added comments in Spiral Matrix solution
    Updated comments for clarity and consistency in time complexity explanation.

--- Matrices/Spiral Matrix (LC - 54).cpp
+++ Matrices/Spiral Matrix (LC - 54).cpp
@@ -1,7 +1,6 @@
-// Time Complexity - O(n*m)
-// where n is the number of rows and m is the number of columns
-// and m*n is the total number of elements in the matrix
-
+//Time Complexity: O(m*n)
+// where m is the no. of rows and n is the no. of columns in the matrix
+// and m*n is the total no. of element present in the matrix
 
 class Solution {
 public:
@@ -14,22 +13,28 @@ public:
         int bottom = m;
         vector<int> res;
         while(top <= bottom && left <= right){
+          
+          // Traverse the Matrix from left to right 
             for(int i = left; i <= right; i++){
                 res.push_back(matrix[top][i]);
             }
-            top++;
+            top++; 
+
+          // Traverse the Matrix from top to bottom 
             for(int i = top; i <= bottom; i++){
                 res.push_back(matrix[i][right]);
             }
             right--;
-
+ 
             if(top <= bottom){
+              // Traverse the Matrix from right to left
                 for(int i = right; i >= left; i--){
                     res.push_back(matrix[bottom][i]);
                 }
                 bottom--;
             }
             if(left <= right){
+              // Traverse the Matrix from bottom to top
                 for(int i = bottom; i >= top; i--){
                     res.push_back(matrix[i][left]);
                 }
